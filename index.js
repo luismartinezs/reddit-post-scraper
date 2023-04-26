8565909ba54ec8b344ba42db0771afc8285e831c
Luis Martinez <luismartinezwebdev@gmail.com>
2023-04-27 00:19:40 +0200

    feat: get all comments from post

--- index.js
+++ index.js
@@ -1,51 +1,101 @@
-const puppeteer = require('puppeteer');
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
 
-const url = 'https://www.reddit.com/r/digitalnomad/comments/12w8yxm';
+function generateOutputPath(url) {
+  const timestamp = new Date().toISOString();
+  const urlSlug = url.split('/').pop();
+  const fileName = `${timestamp}-${urlSlug}.json`;
 
-async function waitForSelectorOrTimeout(page, selector, timeout = 5000) {
-  return Promise.race([
-    page.waitForSelector(selector),
-    new Promise((_, reject) => setTimeout(() => reject(new Error(`Timeout: unable to find ${selector}`)), timeout))
-  ]);
+  const outputDir = 'output';
+
+  if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir);
+  }
+
+  return path.join(outputDir, fileName);
 }
 
-async function scrapeRedditPost(url) {
-  const browser = await puppeteer.launch({ headless: 'new' });
-  const page = await browser.newPage();
-  await page.goto(url, { waitUntil: 'networkidle2' });
+async function fetchCommentsRecursively(commentsData) {
+  const comments = [];
 
-  await waitForSelectorOrTimeout(page, 'div[data-testid="post-content"]');
-  const postTitle = await page.$eval('div[data-testid="post-content"] h1', el => el.innerText);
+  for (const commentData of commentsData) {
+    if (commentData.kind === 't1') {
+      const author = commentData.data.author;
+      const content = commentData.data.body;
 
-  await waitForSelectorOrTimeout(page, 'div[data-testid="post-content"]');
-  const postContent = await page.$eval('div[data-testid="post-content"]', el => el.innerText);
+      if (author !== '[deleted]' || (content !== '[removed]' && content !== '[deleted]')) {
+        comments.push({ author, content });
+      }
 
-  await waitForSelectorOrTimeout(page, 'div[data-testid="comment"]');
-  const comments = await page.$$eval('div[data-testid="comment"]', comments => {
-    return comments.map(comment => {
-      const author = comment.querySelector('a[href^="/user/"]').innerText;
-      const content = comment.querySelector('div[data-testid="comment-text"]').innerText;
+      // Fetch nested comments (replies)
+      if (commentData.data.replies && commentData.data.replies.data.children) {
+        const nestedCommentsData = commentData.data.replies.data.children;
+        comments.push(...(await fetchCommentsRecursively(nestedCommentsData)));
+      }
+    } else if (commentData.kind === 'more') {
+      for (const childId of commentData.data.children) {
+        const response = await axios.get(
+          `https://www.reddit.com/api/info.json?id=t1_${childId}`
+        );
+        const childCommentsData = response.data.data.children;
+        comments.push(...(await fetchCommentsRecursively(childCommentsData)));
+      }
+    }
+  }
 
-      return { author, content };
-    });
-  });
-
-  await browser.close();
-
-  return {
-    postTitle,
-    postContent,
-    comments
-  };
+  return comments;
 }
 
 
-scrapeRedditPost(url)
-  .then(data => {
-    console.log('Post Title:', data.postTitle);
-    console.log('Post Content:', data.postContent);
-    console.log('Comments:', data.comments);
-  })
-  .catch(err => {
-    console.error('Error:', err);
-  });
+
+async function fetchComments(url, after = '') {
+  try {
+    const response = await axios.get(`${url}?limit=100&depth=10&after=${after}`);
+    const commentsData = response.data[1].data.children;
+    const moreCommentsAfter = response.data[1].data.after;
+
+    const comments = await fetchCommentsRecursively(commentsData);
+
+    if (moreCommentsAfter) {
+      const moreComments = await fetchComments(url, moreCommentsAfter);
+      return comments.concat(moreComments);
+    }
+
+    return comments;
+  } catch (error) {
+    console.error('Error fetching comments from Reddit API:', error.message);
+    return [];
+  }
+}
+
+
+async function fetchRedditPost(url) {
+  try {
+    const response = await axios.get(url);
+    const postData = response.data[0].data.children[0].data;
+
+    const postTitle = postData.title;
+    const postContent = postData.selftext;
+
+    const comments = await fetchComments(url);
+
+    const data = {
+      postTitle,
+      postContent,
+      comments
+    };
+
+    const outputPath = generateOutputPath(url);
+    fs.writeFileSync(outputPath, JSON.stringify(data, null, 2));
+    console.log(`Output written to: ${outputPath}`);
+  } catch (error) {
+    console.error('Error fetching data from Reddit API:', error.message);
+  }
+}
+
+
+
+const url = 'https://www.reddit.com/r/digitalnomad/comments/12w8yxm.json';
+fetchRedditPost(url);
+
